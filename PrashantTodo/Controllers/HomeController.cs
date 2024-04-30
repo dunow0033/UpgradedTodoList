c020e16e91809ab5d325ef39080876b8c14f51af
Brian Donnoe <dunow33@hotmail.com>
2024-04-29 22:19:37 -0400

    making more progress on edit

--- PrashantTodo/Controllers/HomeController.cs
+++ PrashantTodo/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using PrashantTodo.Data;
 using PrashantTodo.Models;
 using PrashantTodo.Models.ViewModels;
+using System;
 using System.Diagnostics;
 
 namespace PrashantTodo.Controllers
@@ -41,7 +42,7 @@ namespace PrashantTodo.Controllers
 				Id = todoItem.Id,
 				dateTime = todoItem.dateTime,
 				status = todoItem.status,
-				description = todoItem.description,
+				description = todoItem.description
 			};
 
 			try
@@ -61,14 +62,52 @@ namespace PrashantTodo.Controllers
         [HttpGet("/EditTodo/{id}")]
         public IActionResult EditTodo(int id)
         {
-			var todoItem = _todoDbContext.TodoItems.Find(id);
+			var todoItem = _todoDbContext.TodoItems.FirstOrDefault(x => x.Id == id);
 
-			//if(todoItem == null)
-			//{
-			//	return NotFound();
-			//}
+			if(todoItem != null)
+			{
+				var editTodoRequest = new EditTodoRequest
+				{
+					Id = todoItem.Id,
+					Name = todoItem.Name,
+					dateTime = todoItem.dateTime,
+					status = todoItem.status,
+					description = todoItem.description
+				};
 
-            return View(todoItem);
+                return View(editTodoRequest);
+            }
+
+			return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult SaveEditTodo(EditTodoRequest todoItem)
+        {
+			var todo = new TodoItem
+			{
+				Id = todoItem.Id,
+				Name = todoItem.Name,
+				dateTime = todoItem.dateTime,
+				status = todoItem.status,
+				description = todoItem.description
+
+			};
+
+			var existingTodo = _todoDbContext.TodoItems.Find(todo.Id);
+            
+
+			if(existingTodo != null)
+			{
+				existingTodo.Name = todo.Name;
+				existingTodo.dateTime = todo.dateTime;
+				existingTodo.status = todo.status;
+				existingTodo.description = todo.description;
+
+                _todoDbContext.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ PrashantTodo/Models/ViewModels/EditTodoRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace PrashantTodo.Models.ViewModels
+{
+    public class EditTodoRequest
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime dateTime { get; set; }
+
+        public string status { get; set; }
+
+        public string? description { get; set; }
+    }
+}
